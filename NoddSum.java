7fa218e18f2ae0cb830a251db4eab9ee87a5111f
k4350 <kaushik4350@gmail.com>
2024-06-16 19:41:36 +0530

    the sum of all odd numbers from 1 to n

--- NoddSum.java
+++ NoddSum.java
@@ -1,11 +1,31 @@
+
+import java.util.Scanner;
+
 class NoddSum{
-    void sumOdd(){
-        
-        return;
+    static void sumOdd(int n){
+
+        int sum = 0;
+        for(int i = 1; i<=n; i+=2){
+            sum += i;
+        }
+
+        System.out.println("the sum of all odd numbers from 1 to " + n + " is "+ sum);
+
     }
 
 
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Enter the number: ");
+        int Num = input.nextInt();
+
+        sumOdd(Num);
+
+        input.close();
+
         
     }
 }
